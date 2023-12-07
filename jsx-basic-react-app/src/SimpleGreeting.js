921d9b69becd1c2b91b42c6fc86845aada2f68c1
dttrue <danieltorres@pursuit.org>
2023-12-07 09:29:58 -0500

    Daniels add SimpleGreeting component

---
+++ jsx-basic-react-app/src/SimpleGreeting.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SimpleGreeting = () => {
+    const greeting = 'Hello, welcome to React!'
+
+    return (
+        <h1>{greeting}</h1>
+    )
+}
+
+export default SimpleGreeting
