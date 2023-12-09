503d989f835e75b2e47054b996626064a07741d8
dttrue <danieltorres@pursuit.org>
2023-12-09 14:39:16 -0500

    second commit

--- jsx-basic-react-app/src/SimpleGreeting.js
+++ jsx-basic-react-app/src/SimpleGreeting.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
+// SimpleGreeting function component
 const SimpleGreeting = () => {
+// greeting variable declared and assigned a string
     const greeting = 'Hello, welcome to React!'
 
+// return statement with greeting variable
     return (
-        <h1>{greeting}</h1>
+        // greeting variable wrapped in h1 tag
+        <div>
+             <h1>{greeting}</h1>
+        </div>
+       
     )
 }
 
