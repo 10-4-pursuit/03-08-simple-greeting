7853d9e2fd98339a32a18fe743bd2cec32e2f0ff
athomas227 <andrewthomas@pursuit.org>
2023-12-09 17:03:32 -0500

    finished exercise

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SimpleGreeting from './SimpleGreeting';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>test</h1>
+      <SimpleGreeting />
+    </div>
+  );
+}
+
+export default App;
