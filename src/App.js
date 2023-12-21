b666f5f49f6d575266e83a43c590422c14328347
KyronGreen6 <kyrongreen@pursuit.org>
2023-12-20 23:09:46 -0500

    Add SimpleGreeting component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SimpleGreeting from './SimpleGreeting';
+
+function App() {
+  return (
+    <div className="App">
+      <SimpleGreeting/>
+    </div>
+  );
+}
+
+export default App;
