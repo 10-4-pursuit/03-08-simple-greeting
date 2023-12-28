4a33b94404ca50edb3cbc348c8792ac105df487f
Azmain Shoeb <azmainshoeb@pursuit.org>
2023-12-28 18:45:37 -0500

    Add SimpleGreeting component

---
+++ jsx-basic-react-app/src/App.js
@@ -0,0 +1,12 @@
+import SimpleGreeting from './SimpleGreeting';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SimpleGreeting/>
+    </div>
+  );
+}
+
+export default App;
