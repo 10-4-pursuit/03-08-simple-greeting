60d724992d2fe7002e60cda914e8e28bfcb9cda1
bperalta14 <briannaperalta@pursuit.org>
2024-01-01 15:38:40 -0500

    Add SimpleGreeting component

---
+++ 03-08-greeting/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SimpleGreeting from "./SimpleGreeting";
+
+const app = () => {
+  return (
+    <div>
+      <SimpleGreeting />
+    </div>
+  )
+}
+
+export default app
---
+++ 03-08-greeting/src/SimpleGreeting.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SimpleGreeting = () => {
+    return (
+        <div>Hello, welcome to React!</div>
+    )
+}
+
+export default SimpleGreeting
