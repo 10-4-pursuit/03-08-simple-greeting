717ebb73e5d49a92a450ab5c86a844673c4c9fbd
Jpena101 <jannpena@pursuit.org>
2024-01-18 07:41:52 -0500

    done

---
+++ 03-08-simple-greeting/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SimpleGreeting from './SimpleGreeting';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SimpleGreeting />
+    </div>
+  );
+}
+
+export default App;
