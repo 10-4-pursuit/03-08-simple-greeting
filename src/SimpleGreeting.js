8a76bff8447fd23425299757831bb6e3064719d1
Mikal Din <Mikal Wazeerud-din@pursuit.org>
2024-01-21 14:23:14 -0500

    add SimpleGreeting component

---
+++ src/SimpleGreeting.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SimpleGreeting = () => {
+    return (
+        <div>
+    
+        <p>Hello, welcome to React! </p>
+        
+
+        </div>
+    );
+
+}
+
+export default SimpleGreeting;
